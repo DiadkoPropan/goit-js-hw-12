48ad745f2cee610efcd10ee2c53c36c9ec21a337
DiadkoPropan <arsenij.guba.2016@gmail.com>
2025-04-22 20:01:15 +0300

    d

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,70 +1,49 @@
-import { getImagesByQuery } from './pixabay-api';
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
 
-const gallery = document.querySelector(".gallery");
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-function createGallery(images) {
-  const markup = images.map(image => `
-  <li class="gallery-item">
-    <a class="gallery-link" href="${image.largeImageURL}">
-      <img
-        class="gallery-image"
-        src="${image.webformatURL}"
-        alt="${image.tags}"
-      />
-    </a>
-    <div class="info">
-        <p><b>Likes</b> ${image.likes}</p>
-        <p><b>Views</b> ${image.views}</p>
-        <p><b>Comments</b> ${image.comments}</p>
-        <p><b>Downloads</b> ${image.downloads}</p>
-      </div>
-  </li>`).join("");
 
-  gallery.insertAdjacentHTML('beforeend', markup);
 
-  const lightbox = new SimpleLightbox('.gallery a', {
-    captionsData: 'alt',
-    captionPosition: 'bottom',
-    captionDelay: 250,
-    scrollZoom: true
-  });
 
-  lightbox.refresh();
+export function createGallery(images) {
+    return images.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => `
+        <li class="gallery-item">
+            <a href="${largeImageURL}" class="gallery-link">
+                <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+            </a>
+            <div class="info">
+                <p class="info-item"><b>Likes</b> ${likes}</p>
+                <p class="info-item"><b>Views</b> ${views}</p>
+                <p class="info-item"><b>Comments</b> ${comments}</p>
+                <p class="info-item"><b>Downloads</b> ${downloads}</p>
+            </div>
+        </li>
+    `).join('');
 }
 
-function clearGallery() {
-  const gallery = document.querySelector(".gallery");
-  gallery.innerHTML = '';  
+
+export function clearGallery() {
+    const galleryElement = document.querySelector('.gallery');
+    if (galleryElement) {
+        galleryElement.innerHTML = "";
+    }
 }
 
-function showLoader() {
-  const loader = document.querySelector(".loader"); 
-  loader.classList.add("is-visible");  
-}
-function hideLoader() {
-  const loader = document.querySelector(".loader"); 
-  loader.classList.remove("is-visible"); 
+
+
+export function showLoader() {
+    const loader = document.querySelector('.loader');
+    if (loader) {
+        console.log("loader on");
+        loader.classList.add('visible');
+    }
 }
 
-function showLoadMoreButton() {
-  const MoreBtn = document.querySelector(".MoreBtn"); 
-  MoreBtn.classList.add("is-visible");  
-}
 
-function hideLoadMoreButton(){
-  const MoreBtn = document.querySelector(".MoreBtn"); 
-  MoreBtn.classList.remove("is-visible"); 
-}
 
-export {
-  createGallery,
-  clearGallery,
-  showLoader,
-  hideLoader,
-  showLoadMoreButton,
-  hideLoadMoreButton,
-};
+export function hideLoader() {
+    const loader = document.querySelector('.loader');
+    if (loader) {
+        console.log("loader off");
+        loader.classList.remove('visible');
+    }
+}
